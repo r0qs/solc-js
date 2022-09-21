d9c6b2458a3ceb022f4da3ada41c17cfa5359789
Rodrigo Q. Saramago <rodrigoqsaramago@gmail.com>
2022-09-21 18:15:41 +0200

    Download list only once

--- scripts/update-and-check-binary.sh
+++ scripts/update-and-check-binary.sh
@@ -4,26 +4,30 @@ set -euo pipefail
 
 BASE_URL="https://binaries.soliditylang.org/bin"
 REPO_ROOT="$(dirname "$0")/.."
+LIST_FILE=/tmp/list.json
 
 function fail() {
-    echo -e "$@" >&2
+    echo -e "ERROR: $@" >&2
     exit 1
 }
 
 function check_version() {
+    curl --silent --fail "$BASE_URL/list.json" -o $LIST_FILE
+    [[ ! -f $LIST_FILE ]] && fail "download of release list failed:\n    [url]: $BASE_URL/list.json"
+
     current_version=$(node ./dist/solc.js --version | sed -En 's/^(.*).Emscripten.*/\1/p')
 
     # Retrieve the correspondent released version
     short_version=$(echo "$current_version" | sed -En 's/^([0-9.]+).*\+commit\.[0-9a-f]+.*$/\1/p')
-    release_version=$(curl --silent --fail "$BASE_URL/list.json" | jq --raw-output ".releases | .[\"$short_version\"]" | sed -En 's/^soljson-v(.*).js$/\1/p')
+    release_version=$(cat $LIST_FILE | jq --raw-output ".releases | .[\"$short_version\"]" | sed -En 's/^soljson-v(.*).js$/\1/p')
 
     # check if current version exists as release
-    if [[ $current_version != "$release_version" ]]; then
-        fail "Failed: version mismatch:\n [current]: $current_version\n [release]: $release_version"
+    if [ $current_version != "$release_version" ]; then
+        fail "version mismatch:\n    [current]: $current_version\n    [release]: $release_version"
     fi
 
     current_sha=$(shasum -b -a 256 ./soljson.js | awk '{ print $1 }')
-    release_sha=$(curl -s "$BASE_URL/list.json" | jq --raw-output ".builds[] | select(.longVersion == \"$release_version\") | .sha256" | sed 's/^0x//')
+    release_sha=$(cat $LIST_FILE | jq --raw-output ".builds[] | select(.longVersion == \"$release_version\") | .sha256" | sed 's/^0x//')
 
 	# check if sha matches
 	if [ "${current_sha}" != "${release_sha}" ]; then
@@ -31,9 +35,9 @@ function check_version() {
 	fi
 
     # check if the current version is the latest release
-    latest_version=$(curl -s "$BASE_URL/list.json"  | jq --raw-output ".latestRelease")
+    latest_version=$(cat $LIST_FILE | jq --raw-output ".latestRelease")
     if [ "$short_version" != "$latest_version" ]; then
-        fail "Failed: version is not the latest release:\n [current]: $short_version\n [latest]: $latest_version"
+        fail "version is not the latest release:\n    [current]: $short_version\n    [latest]: $latest_version"
     fi
 }
 
@@ -49,9 +53,6 @@ function check_version() {
 
     check_version
 
-    # Check if current binary works
-    echo "contract C {}" > C.sol
-    node ./dist/solc.js C.sol --bin
-    [[ ! -f C_sol_C.bin ]] && fail "Failed: downloaded binary may not be working properly"
-    rm -f C.sol C_sol_C.bin
+    # cleanup
+    [[ -f $LIST_FILE ]] && rm -f $LIST_FILE
 )
