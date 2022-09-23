8d10d4bea36d4f9713e1642369d99720482e7c17
Rodrigo Q. Saramago <rodrigoqsaramago@gmail.com>
2022-09-23 10:13:03 +0200

    Add verification of the package version

--- scripts/check-binary.sh
+++ scripts/check-binary.sh
@@ -11,7 +11,7 @@ function fail() {
     exit 1
 }
 
-function check_version() {
+function check_release_version() {
     current_version=$1
 
     curl --silent --fail "$BASE_URL/list.json" -o $LIST_FILE
@@ -21,7 +21,7 @@ function check_version() {
     latest_version=$(cat $LIST_FILE | jq --raw-output ".latestRelease")
     release_version=$(cat $LIST_FILE | jq --raw-output ".releases | .[\"$latest_version\"]" | sed --regexp-extended --quiet 's/^soljson-v(.*).js$/\1/p')
 
-    # check if current version is the latest release
+    # Check if current version is the latest release
     if [ $current_version != "$release_version" ]; then
         fail "Version is not the latest release:\n    [current]: $current_version\n    [latest]: $latest_version"
     fi
@@ -29,7 +29,7 @@ function check_version() {
     current_sha=$(shasum --binary --algorithm 256 ./soljson.js | awk '{ print $1 }')
     release_sha=$(cat $LIST_FILE | jq --raw-output ".builds[] | select(.longVersion == \"$release_version\") | .sha256" | sed 's/^0x//')
 
-    # check if sha matches
+    # Check if sha matches
     if [ "$current_sha" != "$release_sha" ]; then
         fail "Checksum mismatch.\n    [current]: $current_sha\n    [release]: $release_sha"
     fi
@@ -39,11 +39,17 @@ function check_version() {
     cd "$REPO_ROOT"
 
     current_version=$(node ./dist/solc.js --version | sed --regexp-extended --quiet 's/^(.*).Emscripten.*/\1/p')
-    check_version $current_version
+
+    # Verify if current version matches the package version.
+    # It already exits with an error if the version mismatch
+    node ./dist/verifyVersion.js
+
+    # Verify if current version is the latest release
+    check_release_version $current_version
     if [ $? -eq 0 ]; then
         echo "solc-js version $current_version is the latest release"
     fi
 
-    # cleanup temp files
+    # Cleanup temp files
     [[ -f $LIST_FILE ]] && rm -f $LIST_FILE
 )
