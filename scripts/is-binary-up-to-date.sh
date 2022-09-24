8db170f3a7eda5e14754733540d9ce41fefa137b
Rodrigo Q. Saramago <rodrigoqsaramago@gmail.com>
2022-09-24 14:55:29 +0200

    Apply suggestions

--- scripts/is-binary-up-to-date.sh
+++ scripts/is-binary-up-to-date.sh
@@ -7,31 +7,31 @@ REPO_ROOT="$(dirname "$0")/.."
 LIST_FILE=$(mktemp -t solc-bin-list-XXXXXX.json)
 
 function fail() {
-    echo -e "ERROR: $@" >&2
+    echo -e "ERROR: $*" >&2
     exit 1
 }
 
 function check_release_version() {
     local current_version="$1"
 
-    curl --silent --fail "$BASE_URL/list.json" -o $LIST_FILE
+    curl --silent --fail "${BASE_URL}/list.json" -o "$LIST_FILE"
     [[ ! -f $LIST_FILE ]] && fail "Download of release list failed:\n    [url]: ${BASE_URL}/list.json"
 
     # Retrieve the latest released version
-    latest_version=$(cat "$LIST_FILE" | jq --raw-output ".latestRelease")
-    release_version=$(cat $LIST_FILE | jq --raw-output ".releases | .[\"$latest_version\"]" | sed --regexp-extended --quiet 's/^soljson-v(.*).js$/\1/p')
+    latest_version_short=$(jq --raw-output ".latestRelease" "$LIST_FILE")
+    latest_version_long=$(jq --raw-output ".releases | .[\"${latest_version_short}\"]" "$LIST_FILE" | sed --regexp-extended --quiet 's/^soljson-v(.*).js$/\1/p')
 
     # Check if current version is the latest release
-    if [[ $current_version != "$release_version" ]]; then
-        fail "Version is not the latest release:\n    [current]: $current_version\n    [latest]: $latest_version"
+    if [[ "$current_version" != "$latest_version_long" ]]; then
+        fail "Version is not the latest release:\n    [current]: ${current_version}\n    [latest]: ${latest_version_short}"
     fi
 
     current_sha=$(shasum --binary --algorithm 256 ./soljson.js | awk '{ print $1 }')
-    release_sha=$(cat $LIST_FILE | jq --raw-output ".builds[] | select(.longVersion == \"$release_version\") | .sha256" | sed 's/^0x//')
+    release_sha=$(jq --raw-output ".builds[] | select(.longVersion == \"${latest_version_long}\") | .sha256" "$LIST_FILE" | sed 's/^0x//')
 
     # Check if sha matches
-    if [[ $current_sha != "$release_sha" ]]; then
-        fail "Checksum mismatch.\n    [current]: $current_sha\n    [release]: $release_sha"
+    if [[ "$current_sha" != "$release_sha" ]]; then
+        fail "Checksum mismatch.\n    [current]: ${current_sha}\n    [release]: ${release_sha}"
     fi
 }
 
